f6c28108df29ec8e194ff785155aab0c702be6a9
Harsh Shandilya <msfjarvis@gmail.com>
2017-04-20 14:05:24 +0530

    KronicBot: Initial commit

---
+++ build.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import configparser
+import json
+import logging
+import os
+import subprocess
+
+from telegram import ChatAction
+from telegram.ext import CommandHandler
+from telegram.ext import Updater
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read('bot.ini')
+
+updater = Updater(token=config['KEYS']['bot_api'])
+path = config['PATH']['path']
+sudo_users = json.loads(config['ADMIN']['sudo'])
+dispatcher = updater.dispatcher
+
+
+def build(bot, update):
+    if update.message.from_user.id in sudo_users:
+        bot.sendChatAction(chat_id=update.message.chat_id,
+                           action=ChatAction.TYPING)
+        bot.sendMessage(chat_id=update.message.chat_id,
+                        text="Building and uploading to the chat")
+        os.chdir(path)
+        build_command = ['./build.sh', '-b']
+        subprocess.call(build_command)
+        filename = "out/" + os.listdir("out")[0]
+        bot.sendDocument(
+            document=open(filename, "rb"),
+            chat_id=update.message.chat_id)
+
+
+build_handler = CommandHandler('build', build)
+
+dispatcher.add_handler(build_handler)
+
+updater.start_polling()
+updater.idle()
+# Paul's ID --> 171119240
